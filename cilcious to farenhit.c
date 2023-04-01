e1ac4ce4cc2b447e7b25d89cb7a7180b2c74805c
Sagarbera <109843689+Sagarbera@users.noreply.github.com>
2023-04-01 10:51:49 +0530

    Add files via upload

---
+++ cilcious to farenhit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+	float c,f;
+	printf("Enter the tempature in farenhit  :");
+	scanf("%f",&f);
+	c=(5*f-160)/9;
+	printf("Temparature in cilcioes is =%f",c);
+	return 0;
+}
